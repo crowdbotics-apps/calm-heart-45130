9649c171f84373be0d1f043d23121ca535aa8ed1
Crowdbotics <team@crowdbotics.com>
2023-12-20 09:16:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-heart-45130",
+  url: "https://calm-heart-45130.botics.co",
+  api: "https://calm-heart-45130.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
